3f4e5eac77ea68d7d565cda716fee65c519dde34
Diego <diegojf38@gmail.com>
2024-02-28 12:35:51 -0300

    Versión Final Encriptador Texto Alura

---
+++ README.md
@@ -0,0 +1,53 @@
+**Encriptador de Texto**
+
+El Encriptador de Texto es una aplicación web diseñada para cifrar y descifrar mensajes de texto de manera sencilla. Esta herramienta utiliza un algoritmo de sustitución simple para ocultar el contenido del mensaje original.
+
+### Características principales:
+
+1. **Encriptación y Desencriptación:** Permite al usuario cifrar un mensaje ingresado en el área de texto, así como también descifrar mensajes previamente encriptados.
+
+2. **Interfaz Intuitiva:** La interfaz de usuario es fácil de usar y cuenta con secciones claras para ingresar texto, mostrar resultados encriptados o descifrados, y botones para realizar las acciones correspondientes.
+
+3. **Compatibilidad con Navegadores:** Funciona en una variedad de navegadores web modernos, lo que permite a los usuarios acceder a la herramienta desde diferentes dispositivos y sistemas operativos.
+
+4. **Responsividad:** La aplicación está diseñada para adaptarse mayormente a diferentes dispositivos móviles.
+
+### Instrucciones de Uso:
+
+1. **Encriptar un Mensaje:** 
+   - Ingresa el texto que deseas encriptar en el área de texto provista.
+   - Haz clic en el botón "Encriptar".
+   - El mensaje encriptado se mostrará en el área designada.
+
+2. **Desencriptar un Mensaje:**
+   - Ingresa el texto encriptado que deseas descifrar en el área de texto.
+   - Haz clic en el botón "Desencriptar".
+   - El mensaje original se mostrará en el área designada.
+
+3. **Copiar Mensaje:** 
+   - Después de desencriptar un mensaje, puedes hacer clic en el botón "Copiar" para copiar el texto descifrado al portapapeles.
+
+### Tecnologías Utilizadas:
+
+- HTML5
+- CSS3 (con técnicas de diseño responsivo)
+- JavaScript
+
+### Notas de Desarrollo:
+
+- El algoritmo de encriptación utilizado es una simple sustitución de caracteres, donde ciertas letras se reemplazan por otras predefinidas.
+- Se utilizó la biblioteca `sweetalert` para mostrar mensajes de alerta personalizados en la interfaz de usuario.
+- La aplicación ha sido probada en una variedad de dispositivos y navegadores para garantizar su compatibilidad y funcionalidad.
+
+### Mejoras Futuras:
+
+
+- Implementación de métodos de encriptación más seguros.
+- Mejora de la interfaz de usuario con elementos visuales adicionales.
+- Soporte para caracteres especiales y diferentes idiomas.
+- Mejora de la responsividad para una experiencia de usuario óptima tanto en dispositivos móviles como de escritorio.
+
+### Autor:
+
+Desarrollado por [Fernandez Diego] - [2024].
+
---
+++ script.js
@@ -0,0 +1,81 @@
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let mensaje = document.getElementById("mensaje");
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let candado = document.getElementById("candado");
+    let encriptado = document.getElementById("encriptado");
+    let desencriptado = document.getElementById("desencriptado");
+    let textoCifrado = "";
+  
+    for (let i = 0; i < texto.length; i++) {
+      switch (texto[i]) {
+        case "e":
+          textoCifrado += "enter";
+          break;
+        case "i":
+          textoCifrado += "imes";
+          break;
+        case "a":
+          textoCifrado += "ai";
+          break;
+        case "o":
+          textoCifrado += "ober";
+          break;
+        case "u":
+          textoCifrado += "ufat";
+          break;
+        default:
+          textoCifrado += texto[i];
+      }
+    }
+  
+    if (texto.length != 0) {
+      mensaje.textContent = textoCifrado;
+      document.getElementById("texto").value = "";
+      encriptado.style.display = "none";
+      desencriptado.style.display = "block";
+      document.getElementById("btn-copiar").style.display = "block";
+    } else {
+      candado.src = "./img/candado.jpg";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent =
+        "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+  
+    
+  }
+  
+  function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let mensaje = document.getElementById("mensaje");
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let candado = document.getElementById("candado");
+    let textoDescifrado = texto
+  
+      .replace(/enter/g, "e")
+      .replace(/imes/g, "i")
+      .replace(/ai/g, "a")
+      .replace(/ober/g, "o")
+      .replace(/ufat/g, "u");
+  
+    if (texto.length != 0) {
+      mensaje.textContent = textoDescifrado;
+    } else {
+      candado.src = "./img/candado.jpg";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent =
+        "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+    document.getElementById("texto").value = "";
+  }
+  
+  function copiar() {
+    let mensaje = document.getElementById("mensaje");
+    let texto = document.getElementById("texto");
+    texto.value = mensaje.textContent;
+  }
+  
